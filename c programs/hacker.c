1321d01ed665c383a7e350d8affc4e7f6a438bb5
yuvraj singh rathore <anujsingh8758614123@gmail.com>
2026-06-14 11:56:05 +0530

    Add files via upload
    my first coding project

---
+++ c programs/hacker.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf ("mera bahiyon behno\n");
+    return 0;
+}
---
+++ c programs/hey.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("thakur sahab ram ram\n");
+    return 0;
+}
